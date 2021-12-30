311367f28e75f46f43170180ac132a2c2a586444
Antonio Robson de Paula <robson.depaula@gmail.com>
2021-12-30 17:05:52 -0300

    Minor adjusts.

--- svc1/src/main/java/com/github/robsondepaula/svc1/HelloController.java
+++ svc1/src/main/java/com/github/robsondepaula/svc1/HelloController.java
@@ -3,14 +3,12 @@ package com.github.robsondepaula.svc1;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping
-    public Map<String, Object> message() {
-        return Map.of("message", "Hello, from Spring!");
+    @GetMapping("/hello")
+    public Map<String, Object> hello() {
+        return Map.of("message", "Hello, from svc1.");
     }
 }
